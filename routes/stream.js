b6732bc57d238cf4b3d2b4ace59120ed25b6dcb1
SENODROOM <136210526+SENODROOM@users.noreply.github.com>
2026-03-15 12:01:46 +0500

    perfect

--- routes/stream.js
+++ routes/stream.js
@@ -1,6 +1,6 @@
 // routes/stream.js
 const router = require("express").Router();
-const { addSSEClient } = require("../lib/queue");
+const { addSSEClient, getRecentLogs } = require("../lib/queue");
 
 router.get("/", (req, res) => {
   res.setHeader("Content-Type",  "text/event-stream");
@@ -8,6 +8,13 @@ router.get("/", (req, res) => {
   res.setHeader("Connection",    "keep-alive");
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.flushHeaders();
+
+  // Replay last 50 logs immediately on connect so frontend catches up
+  const recent = getRecentLogs().slice(0, 50).reverse();
+  recent.forEach(l => {
+    try { res.write(`data: ${JSON.stringify(l)}\n\n`); } catch {}
+  });
+
   const remove = addSSEClient(res);
   req.on("close", remove);
 });
